d7b54ba29739f8a4095896a58c83675e3d5b2076
Jonathan Tuzman <tuzmusic@gmail.com>
2026-04-28 22:21:26 -0400

    Ex 3: Guess slots

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,19 +4,20 @@ import {sample} from '../../utils';
 import {WORDS} from '../../data';
 import GuessInput from "../GuessInput";
 import Guesses from "../Guesses";
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({answer});
 
-let guessId = 0
-
 function Game() {
   const [guesses, setGuesses] = useState([]);
 
   function addGuess(guess) {
-    setGuesses(p => p.concat({guess, id: guessId++}))
+    if (guesses.length < NUM_OF_GUESSES_ALLOWED) {
+      setGuesses(p => p.concat(guess))
+    }
   }
 
   return (
--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {range} from "../../utils";
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
 
 function Guesses({guesses}) {
   return <div className='guess-results'>
-    {guesses.map(({id, guess}) => (
-      <p className='guess' key={id}>{guess}</p>
-    ))}
+    {range(0, NUM_OF_GUESSES_ALLOWED).map((rowInd) => {
+      const guess = guesses[rowInd]
+      return (
+        <p className='guess' key={rowInd}>
+          {range(0, 5).map(i => (
+            <span className='cell'>{guess ? guess[i] : ''}</span>
+          ))}
+        </p>
+      );
+    })}
   </div>;
 }
 
